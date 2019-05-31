6aaeabffb3835f20db0aacdd439ac017e62b8ec8
angela <angelalaramoreira@gmail.com>
2019-05-31 19:53:26 -0400

    t

---
+++ tarea numero3.R
@@ -0,0 +1,29 @@
+
+
+padronElectoral<-(x+y)
+votosSi<-x
+votosNo<-y
+q<-(0.5+1)(x+y)
+
+totalVotos<-function(50,30,80){
+  
+  if(x+y<q){
+  }ifelse(x>0.3(x+y)){
+          "votoSi"
+  }ifelse(y>0.3(x+y)){
+      "votoNo"
+    }
+    if(x+y>q){
+    }ifelse(x>y){
+      "votoSi"
+    }ifelse(x<y){
+      "votoNo"
+    }else{
+      (x=y)
+      "empate"
+    }
+    }
+    }
+    }
+  }
+}
